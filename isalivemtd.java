5d06b3874ebe093e65ee4e289e325738bcc635b6
Nitish kumar <113499545+Nitish-19@users.noreply.github.com>
2024-05-16 16:56:03 +0530

    Add files via upload

---
+++ isalivemtd.java
@@ -0,0 +1,34 @@
+package multithreading;
+
+public class isalivemtd extends Thread {
+
+	 
+	 
+	 public void run() {
+		 
+		 System.out.println("isalive method started");
+		 
+	 }
+		
+			
+		}
+
+
+class y{
+	
+	public static void main(String [] args)
+	{
+		
+		isalivemtd a = new isalivemtd();
+		isalivemtd a2 = new isalivemtd();
+		
+		
+	//	System.out.println(a.isAlive());//thead start ke phle lga denge toh false return krenge
+		a.start();
+		System.out.println(a.isAlive());
+		a2.start();
+		
+		
+	}
+	}
+
